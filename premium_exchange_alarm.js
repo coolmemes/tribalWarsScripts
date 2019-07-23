ade544364853b4e65d48ff5000dfbaac0c7f9e41
Ying-Kai Dang <dang.yingkai@gmail.com>
2019-07-23 03:38:11 +0200

    Sound is now playing the attack sound from TW No third-party requests

--- premium_exchange_alarm.js
+++ premium_exchange_alarm.js
@@ -2,12 +2,12 @@
 // @name         Premium Exchange - Alert Resources
 // @description  Plays horn sound whenever there is something in the stock
 // @author       FunnyPocketBook
-// @version      1.2
+// @version      1.3
 // @include      https://*/game.php*screen=market*
 // @namespace    https://greasyfork.org/users/151096
 // ==/UserScript==
-
-let audio = new Audio("http://soundbible.com/grab.php?id=1598&type=mp3");
+let assetFolder = getComputedStyle(document.querySelector("body")).getPropertyValue("background-image").replace("url(\"", "").replace("/graphic/background/bg-image.jpg\")", ""); // Get the current asset folder
+let audio = new Audio(assetFolder + "/graphic//sound/attack.ogg");
 let woodOld = parseInt(document.getElementById("premium_exchange_stock_wood").textContent);
 let stoneOld = parseInt(document.getElementById("premium_exchange_stock_stone").textContent);
 let ironOld = parseInt(document.getElementById("premium_exchange_stock_iron").textContent);
@@ -22,4 +22,4 @@ let checkRes = setInterval(() => {
     woodOld = parseInt(document.getElementById("premium_exchange_stock_wood").textContent);
     stoneOld = parseInt(document.getElementById("premium_exchange_stock_stone").textContent);
     ironOld = parseInt(document.getElementById("premium_exchange_stock_iron").textContent);
-}, 100);
+}, 100); 
