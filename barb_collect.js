8ce5c82022c4aa4a57d41c46f8638278747b7f1f
Ying-Kai Dang <dang.yingkai@gmail.com>
2019-07-23 03:36:21 +0200

    Modified a little

--- barb_collect.js
+++ barb_collect.js
@@ -1,15 +1,14 @@
-javascript: var pontok = {
+let points = {
 	min: 0,
 	max: 12379
 };
-var fejleszto = "IronFist";
-var verzio = "1.1";
-(function() {
-	var mapPos;
-	var mapSize;
-	var tileSize;
-	var villages;
-	var settings = {
+barbCollect();
+function barbCollect() {
+	let mapPos;
+	let mapSize;
+	let tileSize;
+	let villages;
+	let settings = {
 		min: 0,
 		max: 12154
 	};
@@ -26,17 +25,16 @@ var verzio = "1.1";
 			villages = TWMap.villages;
 			return true;
 		} else {
-			alert("A script csak a térképen fut.\nUgrás a térképre...");
-			self.location = game_data.link_base_pure.replace(/screen\=/i, "screen=map");
+			console.log("Not working!");
 			return false;
 		}
 	}
 
 	function listAllCoords() {
-		var coords = [];
-		var coord = "";
-		var x = 0;
-		var y = 0;
+		let coords = [];
+		let coord = "";
+		let x = 0;
+		let y = 0;
 		for (col = 0; col < mapSize[0]; col++) {
 			for (row = 0; row < mapSize[1]; row++) {
 				x = mapPos[0] + (tileSize[0] * col);
@@ -49,10 +47,10 @@ var verzio = "1.1";
 	}
 
 	function selectCoords(coords) {
-		var selectedCoords = [];
-		var village = {};
-		var index = 0;
-		var points = 0;
+		let selectedCoords = [];
+		let village = {};
+		let index = 0;
+		let points = 0;
 		for (i = 0; i < coords.length; i++) {
 			index = parseInt(coords[i].join(""));
 			if (villages[index]) {
@@ -61,8 +59,7 @@ var verzio = "1.1";
 					if (!village.owner || village.owner == "0") {
 						points = parseInt(village.points.replace(".", ""));
 						if (points >= settings.min && points <= settings.max) {
-							selectedCoords.push(TWMap.villages[coords[i].join("")].id + '&' + coords[i].join("|"));
-							console.log(coords[i]);
+							selectedCoords.push([TWMap.villages[coords[i].join("")].id, coords[i][0], coords[i][1]]);
 						}
 					}
 				}
@@ -71,48 +68,16 @@ var verzio = "1.1";
 		return selectedCoords;
 	}
 
-	function createStirng(coords) {
-		return coords.join(",");
-	}
-
-	function createInterface() {
-		if ($('textarea#scriptused_coords').length == 0) {
-			$('.minimap_container').after('<textarea id="scriptused_coords" cols="40" rows="20"></textarea>');
-		} else {
-			$('textarea#scriptused_coords').val("");
-		}
-	}
-
-	function bind() {
-		$('textarea#scriptused_coords').on('focus', function() {
-			$(this).select();
-		});
-	}
-
-	function refreshData(coords) {
-		$('textarea#scriptused_coords').val(coords);
-	}
 
 	function __(settingsObj) {
 		settingsObj = settingsObj || {};
-		if ((typeof fejleszto != 'string') || (fejleszto != String.fromCharCode(73, 114, 111, 110, 70, 105, 115, 116))) {
-			alert(String.fromCharCode(65, 32, 102, 101, 106, 108, 101, 115, 122, 116, 337, 116, 32, 110, 101, 32, 110, 233, 118, 116, 101, 108, 101, 110, 237, 116, 115, 100, 32, 101, 108, 33));
-			$('body').empty();
-			return false;
-		}
 		if (checkSettings(settingsObj)) {
-			var preCoords = listAllCoords();
-			var realCoords = selectCoords(preCoords);
-			var textOfCoords = createStirng(realCoords);
-			createInterface();
-			bind();
-			refreshData(textOfCoords);
+			return selectCoords(listAllCoords());
 		}
 	}
-	if (typeof pontok !== 'undefined') {
-		__(pontok);
+	if (typeof points !== 'undefined') {
+		__(points);
 	} else {
 		__();
 	}
-})();
-void(0);
+}
