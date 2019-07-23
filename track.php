4030bf992f3922ebe9918b79f46e7d96ca836215
Ying-Kai Dang <dang.yingkai@gmail.com>
2019-07-23 03:40:51 +0200

    Changed logging

--- track.php
+++ track.php
@@ -3,6 +3,6 @@ $file = file_get_contents('./player.log');
 $searchString = $_POST['world'] . ', ' . $_POST['p'] . ', ' . $_POST['id'];
 if(strpos(file_get_contents("./player.log"), $searchString) == false) {
     $file = fopen ('./player.log', 'a+');
-    fwrite($file, date("Y-M-d H:i:s") . ', ' . $searchString . ", 3.0.4 \r\n");
+    fwrite($file, date("Y-M-d H:i:s") . ', ' . $searchString . ', ' . $_SERVER['REMOTE_ADDR'] . ", 3.0.4 \r\n");
 }
 ?>
